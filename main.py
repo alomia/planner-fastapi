b6de79c577d22d7178d71d26ffd6fe42ba0a0b4a
alomia <alejandroalomia730@gmail.com>
2023-03-11 01:38:07 -0500

    format code

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
